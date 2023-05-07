588bbac3479c5f103622ad6c3aea855f933ebc05
FridaPerez <frida_perez@hotmail.com>
2023-05-07 08:41:56 -0600

    cambios diseño y textos

---
+++ ItzelApp/MaterialsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MaterialsViewController.swift
+//  ItzelApp
+//
+//  Created by Frida Pérez on 06/05/23.
+//
+
+import UIKit
+
+class MaterialsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
