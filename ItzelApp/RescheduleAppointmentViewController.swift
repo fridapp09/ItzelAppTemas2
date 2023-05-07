f17bee9ace91f8ddab85191f18fe9494338165c5
FridaPerez <frida_perez@hotmail.com>
2023-05-07 09:42:15 -0600

    asignacion view controller

---
+++ ItzelApp/RescheduleAppointmentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RescheduleAppointmentViewController.swift
+//  ItzelApp
+//
+//  Created by Frida Pérez on 07/05/23.
+//
+
+import UIKit
+
+class RescheduleAppointmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
